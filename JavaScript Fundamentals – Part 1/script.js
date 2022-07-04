4971ec174835310aaf0883733601f3d9e8081010
mdemon420 <mdeamon4@gmail.com>
2022-07-04 19:27:47 +0600

    condition

--- JavaScript Fundamentals – Part 1/script.js
+++ JavaScript Fundamentals – Part 1/script.js
@@ -1,22 +1,26 @@
-let age = 22;
-age = 24;
+// let age = 22;
+// age = 24;
 
-const birthYear = 1998;
+// const birthYear = 1998;
 
 const now = 2022
 let ageEmon = now - 1998;
 let ageLiton = now - 1984;
-// console.log(ageEmon, ageLiton);
+// // console.log(ageEmon, ageLiton);
 
-const firstName = 'Md';
-const lastName = 'Momenuzzaman';
-// console.log(firstName + ' ' + lastName);
+// const firstName = 'Md';
+// const lastName = 'Momenuzzaman';
+// // console.log(firstName + ' ' + lastName);
 
-//Assignment Operator 
-let x = 10 + 10;
-x += 20;   // x + 10 = 40
-x -= 10;  // x - 10 = 30
-x++;     // x + 1 = 31
-x--;   // x - 1 = 30
-x *= 5;
-console.log(x)
+// //Assignment Operator
+// let x = 10 + 10;
+// x += 20;   // x + 10 = 40
+// x -= 10;  // x - 10 = 30
+// x++;     // x + 1 = 31
+// x--;   // x - 1 = 30
+// x *= 5;
+// x /= 15;
+// // console.log(x)
+
+// console.log(ageLiton > ageEmon)
+// console.log(ageLiton < ageEmon)
