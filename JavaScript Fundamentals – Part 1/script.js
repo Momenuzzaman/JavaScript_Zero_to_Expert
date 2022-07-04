414d58ceff0b84d11ea356b91228ed331638ac00
mdemon420 <mdeamon4@gmail.com>
2022-07-04 18:57:58 +0600

    operator

---
+++ JavaScript Fundamentals – Part 1/script.js
@@ -0,0 +1,20 @@
+let age = 22;
+age = 24;
+
+const birthYear = 1998;
+
+const now = 2022
+let ageEmon = now - 1998;
+let ageLiton = now - 1984;
+// console.log(ageEmon, ageLiton);
+
+const firstName = 'Md';
+const lastName = 'Momenuzzaman';
+// console.log(firstName + ' ' + lastName);
+
+let x = 10 + 10;
+x += 20;   // x + 10 = 40
+x -= 10;  // x - 10 = 30
+x++;     // x + 1 = 31
+x--;   // x - 1 = 30
+// console.log(x)
