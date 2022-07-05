0ea06ef1629be56ebca71fdf47c1778d98880d1b
mdemon420 <mdeamon4@gmail.com>
2022-07-06 01:09:55 +0600

    truthy and falsy

--- JavaScript Fundamentals – Part 1/script.js
+++ JavaScript Fundamentals – Part 1/script.js
@@ -93,3 +93,27 @@
 // n = n - 1;
 // console.log(n) 
 
+
+// truthy and falsy
+
+// five falsy  values : 0,null,NaN,undefined,''
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Emon'));
+console.log(Boolean({}));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all")
+}
+else {
+    console.log("You should gate a gob")
+};
+
+let height = 0;
+if (height) {
+    console.log("YaY! Height is defined")
+}
+else {
+    console.log("Height is not defined")
+}
