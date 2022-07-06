25ceb63d2b00c5db92e6cfdda6e7ea27362118c0
mdemon420 <mdeamon4@gmail.com>
2022-07-06 17:11:06 +0600

    function declaration

--- JavaScript Fundamentals – Part 2/script.js
+++ JavaScript Fundamentals – Part 2/script.js
@@ -1,14 +1,24 @@
-function name() {
-    console.log("my name is Emon")
-};
-name();
+// function name() {
+//     console.log("my name is Emon")
+// };
+// name();
 
-function juiceMaker(apple, orange) {
-    const juice = `${apple} Apple and ${orange} Orange juice`
-    return juice;
+// function juiceMaker(apple, orange) {
+//     const juice = `${apple} Apple and ${orange} Orange juice`
+//     return juice;
+// }
+
+// console.log(juiceMaker(4, 0));
+
+// const orangeAndAppleJuice = juiceMaker(10, 9);
+// console.log(orangeAndAppleJuice)
+
+
+// function declaration 
+function calcAge(birthYear) {
+    const currentYEar = 2022;
+    const age = currentYEar - birthYear;
+    return age;
 }
-
-console.log(juiceMaker(4, 0));
-
-const orangeAndAppleJuice = juiceMaker(10, 9);
-console.log(orangeAndAppleJuice)
+const emonAge = calcAge(1998);
+console.log(emonAge);
