6b0bf46de0a51555be05f2f0f82a6cd9b827d23f
mdemon420 <mdeamon4@gmail.com>
2022-07-06 12:04:14 +0600

    challenge 3

---
+++ JavaScript Fundamentals – Part 1/codingChallenge3.js
@@ -0,0 +1,11 @@
+const dolphinScore = (96 + 108 + 89) / 3;
+const koalasScore = (88 + 91 + 110) / 3;
+
+console.log(dolphinScore, koalasScore)
+
+if (dolphinScore > koalasScore) {
+    console.log("dolphins win the game")
+}
+else {
+    console.log("koalas win the game")
+}
