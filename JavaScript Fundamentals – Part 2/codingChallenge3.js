2a72430e49076178d7ef2c9a052cf982b9c8cd2c
mdemon420 <mdeamon4@gmail.com>
2022-07-13 23:43:14 +0600

    done

--- JavaScript Fundamentals – Part 2/codingChallenge3.js
+++ JavaScript Fundamentals – Part 2/codingChallenge3.js
@@ -9,4 +9,21 @@ const mark = {
     }
 };
 
-const 
+const john = {
+    firstName: 'Jhon',
+    lastName: 'Smit',
+    weight: 92,
+    hight: 1.95,
+
+    calcBMI: function () {
+        return this.weight / (this.hight * this.hight);
+    }
+};
+
+if (mark.calcBMI() > john.calcBMI()) {
+    console.log(`${mark.firstName} ${mark.lastName} BMI ${mark.calcBMI()} is higher than ${john.firstName} ${john.lastName} ${john.calcBMI()}`);
+}
+else if (john.calcBMI() > mark.calcBMI()) {
+    console.log(`${john.firstName} ${john.lastName} BMI ${mark.calcBMI()} is higher than ${mark.firstName} ${john.lastName} ${john.calcBMI()}`);
+}
+console.log(mark.calcBMI())
