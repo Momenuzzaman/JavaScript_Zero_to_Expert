ccc0cdd9018a25c97a9aabab8b4f3e1a023a5b50
mdemon420 <mdeamon4@gmail.com>
2022-07-14 15:40:50 +0600

    bonuse

--- JavaScript Fundamentals – Part 2/codingChallenge4.js
+++ JavaScript Fundamentals – Part 2/codingChallenge4.js
@@ -1,4 +1,5 @@
 const bills = [22, 259, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
 const total = [];
 
 const calcTip = (bill) => {
@@ -7,7 +8,21 @@ const calcTip = (bill) => {
 for (let i = 0; i < bills.length; i++) {
     const bill = bills[i];
     const tip = calcTip(bill);
+    tips.push(tip);
     total.push(bill + tip);
     console.log(`bill : ${bill} ,tip : ${tip}`);
 }
 console.log(`total bill : ${total}`);
+
+
+
+const calcAverage = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+};
+console.log(calcAverage(bills));
+console.log(calcAverage(tips));
+console.log(calcAverage(total));
