1841680292132dba2fbf3c5c4fe0a4a9df5bf61c
mdemon420 <mdeamon4@gmail.com>
2022-07-15 11:50:34 +0600

    add min

--- Developer-Skills/script.js
+++ Developer-Skills/script.js
@@ -5,12 +5,17 @@ const temperatures = [2, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 const calcTemp = (temps) => {
     let max = temps[0];
+    let min = temps[0];
     for (let i = 1; i < temps.length; i++) {
-        if (temps[i] > max) {
+        const temp = temps[i];
+        if (temp > max) {
             max = temps[i];
         }
+        if (temp < min) {
+            min = temp;
+        }
     }
-    console.log(max);
+    console.log(max, min);
 };
 calcTemp(temperatures);
 
