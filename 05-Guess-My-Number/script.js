118c57146dca28506ac73d9b2027f72e275be25d
mdemon420 <mdeamon4@gmail.com>
2022-07-20 16:43:15 +0600

    add function

---
+++ 05-Guess-My-Number/script.js
@@ -0,0 +1,13 @@
+// document.querySelector(".number").textContent = 20;
+// console.log(document.querySelector(".number").textContent = 20);
+
+// document.querySelector(".guess").value = 10;
+// console.log(document.querySelector(".guess").value);
+
+// document.querySelector(".message").textContent = "Correct Number";
+// console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".check").addEventListener("click", function () {
+    const guess = document.querySelector(".guess").value;
+    console.log(guess);
+});
