46663cb746eb9cfa513f1934f232f5aa4c336484
mdemon420 <mdeamon4@gmail.com>
2022-07-20 16:45:46 +0600

    string convert to number

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -8,6 +8,6 @@
 // console.log(document.querySelector(".message").textContent);
 
 document.querySelector(".check").addEventListener("click", function () {
-    const guess = document.querySelector(".guess").value;
+    const guess = Number(document.querySelector(".guess").value);
     console.log(guess);
 });
