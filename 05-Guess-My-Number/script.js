e10a838c581750d84d1755c1bdf50385c4988807
mdemon420 <mdeamon4@gmail.com>
2022-07-20 16:48:18 +0600

    if statment add

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -9,5 +9,8 @@
 
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
-    console.log(guess);
+
+    if (!guess) {
+        document.querySelector(".message").textContent = "Not a valid number";
+    }
 });
