747bba9a0d90cad31bcf000d8732b73a7408f1a3
mdemon420 <mdeamon4@gmail.com>
2022-07-27 18:50:47 +0600

    all class add

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,4 @@
-
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnOpenModal = document.querySelector(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
