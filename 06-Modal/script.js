e9f59b754c8fd7731ef3a0ba2e4d9e11dd6c2fc7
mdemon420 <mdeamon4@gmail.com>
2022-07-27 20:46:21 +0600

    add overlay

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -16,7 +16,4 @@ for (let i = 0; i < btnOpenModal.length; i++) {
 
 }
 btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-});
+overlay.addEventListener("click", closeModal);
