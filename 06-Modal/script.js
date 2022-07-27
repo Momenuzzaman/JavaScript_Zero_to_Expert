9ff2b83c05cd16c93acf03077fa288bf694522d8
mdemon420 <mdeamon4@gmail.com>
2022-07-27 20:48:39 +0600

    create open model function

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -3,16 +3,17 @@ const overlay = document.querySelector(".overlay");
 const btnOpenModal = document.querySelectorAll(".show-modal");
 const btnCloseModal = document.querySelector(".close-modal");
 
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
 const closeModal = function () {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
 };
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-    btnOpenModal[i].addEventListener("click", function () {
-        modal.classList.remove("hidden");
-        overlay.classList.remove("hidden");
-    });
+    btnOpenModal[i].addEventListener("click", openModal);
 
 }
 btnCloseModal.addEventListener("click", closeModal);
