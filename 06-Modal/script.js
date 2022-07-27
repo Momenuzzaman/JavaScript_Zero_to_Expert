319bdc14aee28963e856092f0e9f84e1dd37639a
mdemon420 <mdeamon4@gmail.com>
2022-07-27 19:22:26 +0600

    for loop run

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,4 +1,8 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnOpenModal = document.querySelector(".show-modal");
-const btnCloseModal = document.querySelectorAll(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+    console.log(btnOpenModal[i]);
+}
