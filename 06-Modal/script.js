fd5cd9569ac4bad0f97e55d65026417e3be04c6d
mdemon420 <mdeamon4@gmail.com>
2022-07-27 20:44:49 +0600

    create  new funtion

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -3,16 +3,19 @@ const overlay = document.querySelector(".overlay");
 const btnOpenModal = document.querySelectorAll(".show-modal");
 const btnCloseModal = document.querySelector(".close-modal");
 
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
 for (let i = 0; i < btnOpenModal.length; i++) {
     btnOpenModal[i].addEventListener("click", function () {
         modal.classList.remove("hidden");
         overlay.classList.remove("hidden");
     });
-    btnCloseModal.addEventListener("click", function () {
-        modal.classList.add("hidden");
-        overlay.classList.add("hidden");
-    });
+
 }
+btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", function () {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
