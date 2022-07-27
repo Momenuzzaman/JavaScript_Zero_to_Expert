6a7e85858f477f02c4d158fbb496811737cd883a
mdemon420 <mdeamon4@gmail.com>
2022-07-27 19:01:41 +0600

    querySelectorAll add

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,4 +1,4 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnOpenModal = document.querySelector(".show-modal");
-const btnCloseModal = document.querySelector(".close-modal");
+const btnCloseModal = document.querySelectorAll(".close-modal");
