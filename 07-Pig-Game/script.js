82eac7ba610b976bbe20dd4cd07683301ede1b1c
mdemon420 <mdeamon4@gmail.com>
2022-08-06 13:00:24 +0600

    dice create

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -2,6 +2,7 @@
 
 const score0 = document.querySelector("#score--0");
 const score1 = document.querySelector("#score--1");
+const dice = document.querySelector(".dice");
 
 score0.textContent = 0;
 score1.textContent = 0;
