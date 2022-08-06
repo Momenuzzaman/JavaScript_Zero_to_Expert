6f208a22710eba6e577a7624d23da9075cb7a5c8
mdemon420 <mdeamon4@gmail.com>
2022-08-07 01:29:58 +0600

    player add

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,5 +1,6 @@
 // selecting Elements 
-
+const player0 = document.querySelector(".player player--0");
+const player1 = document.querySelector(".player player--1");
 const score0 = document.querySelector("#score--0");
 const currentScore0 = document.querySelector("#current--0");
 const score1 = document.querySelector("#score--1");
@@ -32,5 +33,6 @@ btnRoll.addEventListener("click", function () {
         document.querySelector(`#current--${activePlayer}`).textContent = 0;
         currentScore = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
+
     }
 });
