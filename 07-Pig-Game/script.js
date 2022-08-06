2b5a3616de541c6d1cf8dbbc4e3b62dfdc7868da
mdemon420 <mdeamon4@gmail.com>
2022-08-07 01:34:26 +0600

    add active class

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,6 +1,6 @@
 // selecting Elements 
-const player0 = document.querySelector(".player player--0");
-const player1 = document.querySelector(".player player--1");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
 const score0 = document.querySelector("#score--0");
 const currentScore0 = document.querySelector("#current--0");
 const score1 = document.querySelector("#score--1");
@@ -33,6 +33,7 @@ btnRoll.addEventListener("click", function () {
         document.querySelector(`#current--${activePlayer}`).textContent = 0;
         currentScore = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
-
+        player0.classList.toggle("player--active");
+        player1.classList.toggle("player--active");
     }
 });
