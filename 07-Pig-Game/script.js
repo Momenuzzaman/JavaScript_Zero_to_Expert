8898e40a59587c4ef2ccad8d9cce73902654fd1d
mdemon420 <mdeamon4@gmail.com>
2022-08-06 21:44:36 +0600

    btn hold

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -5,6 +5,7 @@ const score1 = document.querySelector("#score--1");
 const dice = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // stating condition
 score0.textContent = 0;
