fe070afee8a655d58570a2a324280e99ffc1cfe7
mdemon420 <mdeamon4@gmail.com>
2022-08-06 21:42:10 +0600

    btn New add

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -3,6 +3,7 @@
 const score0 = document.querySelector("#score--0");
 const score1 = document.querySelector("#score--1");
 const dice = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
 
 // stating condition
 score0.textContent = 0;
