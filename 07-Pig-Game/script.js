1bf69f0542f53fbd13ce287e6d75d479a79e56eb
mdemon420 <mdeamon4@gmail.com>
2022-08-07 16:39:52 +0600

    dice hidden add

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -48,6 +48,7 @@ btnHold.addEventListener("click", function () {
         scores[activePlayer] += currentScore;
         document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
         if (scores[activePlayer] >= 20) {
+            dice.classList.add("hidden");
             playing = false;
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
