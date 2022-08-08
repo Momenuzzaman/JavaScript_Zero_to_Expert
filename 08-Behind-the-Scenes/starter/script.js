3d402a4531fa486e7bf8c12d5e93d33af7144dfe
mdemon420 <mdeamon4@gmail.com>
2022-08-08 23:23:06 +0600

    detail fuction call

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -2,11 +2,13 @@ const name = "Emon";
 
 function calcAge(birthYear) {
     const age = 2022 - birthYear;
-    return age;
 
     function detail() {
-        const output = `M`
+        const output = `My Name ${name}. Age ${age} Born ${birthYear}`;
+        console.log(output);
     }
+    detail();
+    return age;
 };
 
 const result = calcAge(1998);
