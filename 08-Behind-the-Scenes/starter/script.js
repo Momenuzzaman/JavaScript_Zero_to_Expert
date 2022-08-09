13e14f51fba8d09bef2c45a9a6041db7ffe81a4a
mdemon420 <mdeamon4@gmail.com>
2022-08-09 14:00:27 +0600

    condition

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,15 +1,24 @@
-const name = "Emon";
+// const name = "Emon";
 
-function calcAge(birthYear) {
-    const age = 2022 - birthYear;
+// function calcAge(birthYear) {
+//     const age = 2022 - birthYear;
 
-    function detail() {
-        const output = `My Name ${name}. Age ${age} Born ${birthYear}`;
-        console.log(output);
-    }
-    detail();
-    return age;
-};
+//     function detail() {
+//         const output = `My Name ${name}. Age ${age} Born ${birthYear}`;
+//         console.log(output);
+//     }
+//     detail();
+//     return age;
+// };
 
-const result = calcAge(1998);
-console.log(result);
+// const result = calcAge(1998);
+// console.log(result);
+
+const product = 10;
+
+const deleteProduct = () => {
+    console.log("All products deleted");
+}
+if (product < 15) {
+    deleteProduct();
+}
