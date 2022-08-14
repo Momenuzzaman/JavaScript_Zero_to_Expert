3625db05aeca9e545ed208da88084425a9d46168
mdemon420 <mdeamon4@gmail.com>
2022-08-14 13:26:12 +0600

    create a object restaurant

---
+++ 09-Data-Structures-Operators/script.js
@@ -0,0 +1,7 @@
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
