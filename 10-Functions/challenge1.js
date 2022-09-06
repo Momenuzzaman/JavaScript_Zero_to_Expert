8bdfc0c45b19cf0675ba6788bfbfca8d63405c0b
mdemon420 <mdeamon4@gmail.com>
2022-09-07 02:13:45 +0600

    count++

--- 10-Functions/challenge1.js
+++ 10-Functions/challenge1.js
@@ -1,16 +1,27 @@
 const poll = {
     question: "What is your favourite programming language?",
-    options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+    options: ["0: JavaScript",
+        "1: Python",
+        "2: Rust",
+        "3: C++"
+    ],
     // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
     registerNewAnswer: function () {
-        answers = Number(
+        answer = Number(
             prompt(`${this.options}
         ${this.options}
         ('Write option number')
         `)
         );
-        console.log(answers);
+        console.log(answer);
+
+        // register
+        typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++;
+        console.log(this.answers);
     }
+
 };
-poll.registerNewAnswer();
+// poll.registerNewAnswer();
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
