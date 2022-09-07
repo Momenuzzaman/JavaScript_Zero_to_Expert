3d48c75d21e7a1af5d14dd60167e6142e58bca8a
mdemon420 <mdeamon4@gmail.com>
2022-09-07 13:10:27 +0600

    closures

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -47,15 +47,15 @@
 
 // greed('hello')('Liton');
 
-const lufthansa = {
-    ariLine: 'Lufthansa',
-    iataCode: 'LH',
-    bookings: [],
-    book(flightNum, name) {
-        console.log(`${name} booked a seat on ${this.ariLine} flight ${this.iataCode}${flightNum}`);
-        this.bookings.push({ flight: `${this.iataCode} ${flightNum},${name}` });
-    }
-};
+// const lufthansa = {
+//     ariLine: 'Lufthansa',
+//     iataCode: 'LH',
+//     bookings: [],
+//     book(flightNum, name) {
+//         console.log(`${name} booked a seat on ${this.ariLine} flight ${this.iataCode}${flightNum}`);
+//         this.bookings.push({ flight: `${this.iataCode} ${flightNum},${name}` });
+//     }
+// };
 // const bangladeshBiman = {
 //     ariLine: 'BangladeshBiman',
 //     iataCode: 'BHB',
@@ -72,11 +72,25 @@ const lufthansa = {
 // const data = [222, 'Monir'];
 // book.apply(bangladeshBiman, data);
 
-lufthansa.planes = 200;
-lufthansa.byePlane = function () {
-    this.planes++;
-    console.log(this.planes);
-};
-// lufthansa.byePlane();
+// lufthansa.planes = 200;
+// lufthansa.byePlane = function () {
+//     this.planes++;
+//     console.log(this.planes);
+// };
+// // lufthansa.byePlane();
 
-document.querySelector('.buy').addEventListener('click', lufthansa.byePlane.bind(lufthansa));
+// document.querySelector('.buy').addEventListener('click', lufthansa.byePlane.bind(lufthansa));
+
+
+// closures************
+const booking = () => {
+    let passenger = 0;
+    return function () {
+        passenger++;
+        console.log(`${passenger} passenger`);
+    };
+};
+
+const book = booking();
+book();
+book();
