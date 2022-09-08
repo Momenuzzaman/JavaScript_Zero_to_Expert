97d5a81b1781acdbe4ca3fb8dbb91b2f4d9fa48a
mdemon420 <mdeamon4@gmail.com>
2022-09-08 17:56:23 +0600

    slice

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -73,3 +73,10 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -2));
