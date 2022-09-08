cacfb7f54c809716f33bdbe9709136f5015bc281
mdemon420 <mdeamon4@gmail.com>
2022-09-09 04:23:42 +0600

    create a function displayMovement

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -60,6 +60,21 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+
+const displayMovements = (movements) => {
+  movements.forEach(function (movement, index) {
+    const type = movement > 0 ? 'deposit' : 'withdrawal'
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${index + 1}</div>
+        <div class="movements__value">${movement}</div>
+    </div>
+`;
+  });
+};
+
+displayMovements(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
