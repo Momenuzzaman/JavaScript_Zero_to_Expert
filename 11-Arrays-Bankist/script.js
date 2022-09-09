a70c4457654c527e251fa61b1328fbcdbc9cc579
mdemon420 <mdeamon4@gmail.com>
2022-09-09 15:34:48 +0600

    forEach adult and puppy dog

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -154,9 +154,15 @@ const checkDogs = (dogsJulia, dogsKate) => {
   const dogJuliaCorrect = dogsJulia.slice();
   dogJuliaCorrect.splice(0, 1);
   dogJuliaCorrect.splice(-2);
-  console.log(dogJuliaCorrect);
-  const dog = [...dogJuliaCorrect, ...dogsKate];
-  console.log(dog);
+
+  const dogs = [...dogJuliaCorrect, ...dogsKate];
+
+  dogs.forEach(function (dog, i) {
+    dog >= 3 ?
+      console.log(`Dog number ${i + 1} is an adult,and is ${dog} year old`)
+      : console.log(`dog number ${i + 1} is still puppy`);
+  });
 
 };
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
