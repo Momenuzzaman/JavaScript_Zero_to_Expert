6718019222f50061e79ab153e1fa45d608c4a12f
mdemon420 <mdeamon4@gmail.com>
2022-09-10 17:48:22 +0600

    delete account

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -158,6 +158,20 @@ btnLogin.addEventListener('click', function (e) {
     updateUI();
   });
 });
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (inputCloseUsername.value === currentAccount.userName && Number(inputClosePin.value) === currentAccount.pin) {
+    const index = accounts.findIndex(account => account.userName === currentAccount.userName);
+    console.log(index);
+    // delete account 
+    accounts.splice(index, 1);
+    // hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
