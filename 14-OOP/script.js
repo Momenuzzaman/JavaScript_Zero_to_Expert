5d34c32f9dfa59515ed38f50c4fbbee1bd023054
mdemon420 <mdeamon4@gmail.com>
2022-09-15 21:09:25 +0600

    prototype

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,17 @@
+const person = function (name, birthYear) {
+    this.name = name;
+    this.birthYear = birthYear;
+};
+
+const emon = new person('Emon', 1998);
+console.log(emon);
+const liton = new person('Liton', 1984);
+console.log(liton);
+
+person.prototype.calcAge = function () {
+    console.log(2022 - this.birthYear);
+};
+
+emon.calcAge();
+liton.calcAge();
 
