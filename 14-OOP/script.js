c952536424b4d3226b9543a9caf2d632f149e1d2
mdemon420 <mdeamon4@gmail.com>
2022-09-15 22:48:25 +0600

    class constructor

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -15,29 +15,51 @@
 // emon.calcAge();
 // liton.calcAge();
 
-const Car = function (make, speed) {
-    this.make = make;
-    this.speed = speed;
-};
 
-const bmw = new Car('BMW', 120);
-const mercedes = new Car('Mercedes', 95);
+// challenge 1
+// const Car = function (make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-    this.speed += 10;
-    console.log(`${this.make} speed of the car in ${this.speed} km/h`);
-};
-Car.prototype.brake = function () {
-    this.speed -= 5;
-    console.log(`${this.make} speed of the car in ${this.speed} km/h`);
-};
-bmw.accelerate();
-bmw.accelerate();
-bmw.brake();
-bmw.accelerate();
-console.log("----------");
-mercedes.accelerate();
-mercedes.accelerate();
-mercedes.accelerate();
-mercedes.brake();
-mercedes.accelerate();
+// const bmw = new Car('BMW', 120);
+// const mercedes = new Car('Mercedes', 95);
+
+// Car.prototype.accelerate = function () {
+//     this.speed += 10;
+//     console.log(`${this.make} speed of the car in ${this.speed} km/h`);
+// };
+// Car.prototype.brake = function () {
+//     this.speed -= 5;
+//     console.log(`${this.make} speed of the car in ${this.speed} km/h`);
+// };
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.brake();
+// bmw.accelerate();
+// console.log("----------");
+// mercedes.accelerate();
+// mercedes.accelerate();
+// mercedes.accelerate();
+// mercedes.brake();
+// mercedes.accelerate();
+
+class Person {
+    constructor(name, birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+    calAge() {
+        console.log(2022 - this.birthYear);
+    }
+    greet() {
+        console.log(`My name is ${this.name}`);
+    }
+}
+const liton = new Person('Liton', 1984);
+const emon = new Person('Emon', 1998);
+
+console.log(emon, liton);
+emon.calAge();
+emon.greet();
+
