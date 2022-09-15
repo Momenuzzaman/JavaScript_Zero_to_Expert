d30db0ac4d9ffcf8cdef0249799303277bd48230
mdemon420 <mdeamon4@gmail.com>
2022-09-15 21:20:07 +0600

    car accelerate

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,17 +1,31 @@
-const person = function (name, birthYear) {
-    this.name = name;
-    this.birthYear = birthYear;
+// const person = function (name, birthYear) {
+//     this.name = name;
+//     this.birthYear = birthYear;
+// };
+
+// const emon = new person('Emon', 1998);
+// console.log(emon);
+// const liton = new person('Liton', 1984);
+// console.log(liton);
+
+// person.prototype.calcAge = function () {
+//     console.log(2022 - this.birthYear);
+// };
+
+// emon.calcAge();
+// liton.calcAge();
+
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
 };
 
-const emon = new person('Emon', 1998);
-console.log(emon);
-const liton = new person('Liton', 1984);
-console.log(liton);
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
 
-person.prototype.calcAge = function () {
-    console.log(2022 - this.birthYear);
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.car} speed of the car in ${this.speed} km/h`);
 };
-
-emon.calcAge();
-liton.calcAge();
-
+bmw.accelerate();
+bmw.accelerate();
