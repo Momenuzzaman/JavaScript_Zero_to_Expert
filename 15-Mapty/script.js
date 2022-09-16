70447f0062b81fe49c37cb9bbb26a7f6ad285195
mdemon420 <mdeamon4@gmail.com>
2022-09-16 19:48:41 +0600

    leaflet map add .

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -16,6 +16,16 @@ if (navigator.geolocation)
         const { latitude } = position.coords;
         const { longitude } = position.coords;
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+        const map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
     },
         function () {
             alert('Could not get your position');
