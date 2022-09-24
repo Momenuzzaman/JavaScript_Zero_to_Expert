7bc5d154c25c61d5552d7104f8c3a3e65b2dc022
mdemon420 <mdeamon4@gmail.com>
2022-09-24 12:33:45 +0600

    promise

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -2,3 +2,6 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
