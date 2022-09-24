67ad46645f1702a2f7f58bb0065b94d3663e774e
mdemon420 <mdeamon4@gmail.com>
2022-09-24 12:36:58 +0600

    promise 2

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -5,3 +5,9 @@ const countriesContainer = document.querySelector('.countries');
 console.log('Test start');
 setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+    for (let i = 0; i < 10000; i++) {
+        console.log(res);
+    }
+});
