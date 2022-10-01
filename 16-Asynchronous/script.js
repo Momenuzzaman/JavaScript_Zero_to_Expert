d50aa96a6ac0e0e8fc5e2399802d1c5472c6b814
mdemon420 <mdeamon4@gmail.com>
2022-10-01 23:59:20 +0600

    comment

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -50,9 +50,9 @@ const countriesContainer = document.querySelector('.countries');
 //     .catch(err => console.error(err));
 
 
-const countryDetails = async function (country) {
-    const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
-    const data = await res.json();
-    console.log(data);
-};
-countryDetails('bangladesh');
+// const countryDetails = async function (country) {
+//     const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+//     const data = await res.json();
+//     console.log(data);
+// };
+// countryDetails('bangladesh');
